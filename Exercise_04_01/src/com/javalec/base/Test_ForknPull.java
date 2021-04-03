50956c5bfaaf67600d585c834e50691964a9ff96
norsk-Gh <81655952+norsk-Gh@users.noreply.github.com>
2021-04-03 23:51:29 +0900

    test_newclass
    test for pulling

---
+++ Exercise_04_01/src/com/javalec/base/Test_ForknPull.java
@@ -0,0 +1,12 @@
+package com.javalec.base;
+
+public class Test_ForknPull {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Can you find me?");
+		
+	}
+
+}
