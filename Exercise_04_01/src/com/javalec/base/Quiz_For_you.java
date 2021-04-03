5a522222cfd459e75f888c30efea3bbb99d526e7
norsk-Gh <81655952+norsk-Gh@users.noreply.github.com>
2021-04-04 00:19:54 +0900

    quiz

---
+++ Exercise_04_01/src/com/javalec/base/Quiz_For_you.java
@@ -0,0 +1,10 @@
+package com.javalec.base;
+
+public class Quiz_For_you {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("What is this?");
+	}
+
+}
